510390456f44a3158bd8c74a9be1770e79efd243
patty-13 <56751154+patty-13@users.noreply.github.com>
2022-03-15 11:58:35 +0530

    approach 3

--- TREES/count_node.cpp
+++ TREES/count_node.cpp
@@ -1,6 +1,7 @@
 #include "iostream"
 #include "algorithm"
 #include "queue"
+#include "math.h"
 using namespace std;
 struct node
 {
@@ -13,6 +14,8 @@ struct node
     }
 };
 int counter = 0;
+
+// using extra space + log(n), recursion.
 int count_node(node *root, queue<int> &q)
 {
     if(root != NULL)
@@ -26,6 +29,7 @@ int count_node(node *root, queue<int> &q)
     return q.size();
 
 }
+// using no extra space time complexity log(n)
 int approach_2(node *root)
 {
     if(root == NULL){return 0;}
@@ -34,18 +38,27 @@ int approach_2(node *root)
         return 1 + approach_2(root->left) + approach_2(root->right);
     }
 }
-// int count_node(node *root)
-// {
-//     if(root == NULL){return 0;}
-//     queue<node *> q;
-//     q.push(root);
-//     while(q.empty() == false)
-//     {   node *curr = q.front();
-//         q.pop();
-//         if(curr->left != NULL){q.push(curr->left);}
-//         if(curr->right != NULL){q.push(curr -> right);}
-//     }
-// }
+// using time complexity log(n) * log(n);
+int approach_3(node *root)
+{
+    int lh = 0, rh = 0;
+    node *curr = root;
+    while(curr != NULL)
+    {
+        lh++;
+        curr = curr->left;
+    }   
+    curr = root;
+    while (curr != NULL)
+    {
+        rh++;
+        curr = curr->right;
+    }
+    if(rh == lh) {return pow(2,lh)-1;}
+    else {return 1 + approach_3(curr->left)+approach_3(curr->right);}
+    
+
+}
 int main()
 {
     queue<int> q;
